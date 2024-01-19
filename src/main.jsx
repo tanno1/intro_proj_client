bab82aa6d144586792f5b25a998c3adc40f50cf8
Noah Tanner <noahetanner@gmail.com>
2024-01-18 22:30:12 -0800

    created rect-router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
+import { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom';
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Root from "./views/Root";
+import './App.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
 )
