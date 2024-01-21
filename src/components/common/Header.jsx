a7d1a075e17aab644227cdd513d4bb68b8913793
Noah Tanner <noahetanner@gmail.com>
2024-01-21 11:59:17 -0800

    added basic tailwind styling to header bar

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,13 +3,13 @@ import pfp from "./Untitled.jpg"
 
 const Header = () => {
     return (
-        <header className="bg-blue-">
+        <header className="bg-myLightGray text-black p-4 flex justify-between items-center">
             <div>
-                <h1>routes.ai</h1>
+                <h1 className='text-2xl'>routes.ai</h1>
             </div>
-            <div>
-                <p>my routes</p>
-                <img src={pfp} alt="profile photo" />
+            <div className="flex items-center space-x-4">
+                <p className='text-lg'>my routes</p>
+                <img src={pfp} alt="profile photo" className="w-10 h-10 rounded-full"/>
             </div>
         </header>
     )
