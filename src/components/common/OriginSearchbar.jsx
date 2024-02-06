08fbbe4791ee5af1a0f46816f6af93e91fc97830
Noah Tanner <noahetanner@gmail.com>
2024-02-05 21:24:11 -0800

    asd;lkjfhabhlsjkdf

---
+++ src/components/common/OriginSearchbar.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useTripContext } from '../../context/tripcontext';
+
+const  OriginSeachbar = ({ placeholder }) => {
+  const { origin, setOrigin } = useTripContext();
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    console.log('origin', origin)
+  }, [origin])
+
+  const onSubmit = (event)=> {
+    event.preventdefault()
+    setOrigin(event.target.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="flex justify-between w-1/5 rounded-xl">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={inputValue}
+          onChange={() => setInputValue(e.target.value)}
+          className="w-fit p-2 outline-none rounded-xl bg-myGray"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default OriginSeachbar;
+ 
